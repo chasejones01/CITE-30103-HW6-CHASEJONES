9f78becea13abbd40b5b7eba5ea5786df67f2ff9
chasejones01 <chase.jones@tcu.edu>
2025-11-14 03:06:13 -0600

    Q1: Calculates the area and perimeter of a rectangle

---
+++ question1.c
@@ -0,0 +1,36 @@
+/* Documentation Section:
+Program: Calculate the  Area and Perimeter of a Rectangle.
+Date: 11-09-2025
+*/
+
+/* Link Section */
+#include <stdio.h>
+
+/* Defintion Section */
+// None needed for this program
+
+/* Main Function Section */
+int main(void) {
+	double length, width; // variables
+	
+	// Ask user for input
+	printf("Enter length: ");
+	if(scanf("%lf", &length) != 1) { // validate length input
+		printf("Invalid length input.\n"); // Print error message if invalid & exits with return 1
+		return 1;
+	}
+	printf("Enter width: ");
+	if(scanf("%lf", &width) != 1) {	// validate width input
+		printf("Invalid width input.\n");
+		return 1;
+	}
+
+	// Define area & perimeter variables
+	double area = length * width;
+	double perimeter = 2 * (length + width);
+
+	printf("Area of Rectangle: %lf\n", area);
+	printf("Perimeter of Rectangle: %lf\n", perimeter);
+
+	return 0;
+}
